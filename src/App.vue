c998afa217a379d4046bf63c84554f1595d6e39c
eightdoor <851708184@qq.com>
2021-09-27 23:22:41 +0800

    chore: commit 代码提交规范

--- src/App.vue
+++ src/App.vue
@@ -19,7 +19,7 @@ export default defineComponent({
     const router = useRouter()
     const store = useStore()
     onMounted(() => {
-      // 监听键盘快捷键
+      // 监听键盘快捷键 是否触发
       ipcRenderer.on("BaiduTranslate", (event, arg) => {
         console.log('text', arg)
         store.commit('MenuBar/setIndex', '百度翻译')
