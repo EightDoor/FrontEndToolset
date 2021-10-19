721a5b31b17637aa99b06e2e71558560be95ff94
EightDoor <851708184@qq.com>
2021-10-19 15:32:17 +0800

    fix

--- electron/events/open_browser.js
+++ electron/events/open_browser.js
@@ -2,8 +2,8 @@ const { BrowserView } = require('electron')
 
 
 module.exports = (win) => {
-  const view = new BrowserView()
-  win.setBrowserView(view)
-  view.setBounds({ x: 0, y: 0, width: 300, height: 300 })
-  view.webContents.loadURL('https://electronjs.org')
+  // const view = new BrowserView()
+  // win.setBrowserView(view)
+  // view.setBounds({ x: 0, y: 0, width: 300, height: 300 })
+  // view.webContents.loadURL('https://electronjs.org')
 }
