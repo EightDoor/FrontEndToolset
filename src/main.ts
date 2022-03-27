693d4259b36786e6cd5f0cbcb16874bb41f15790
EightDoor <zhou851708184@163.com>
2022-03-27 22:46:46 +0800

    报错解决

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import App from './App.vue';
 
 // element-plus
 import ElementPlus from 'element-plus';
-import 'element-plus/dist/index.css';
+import 'element-plus/theme-chalk/index.css';
 import zhCn from 'element-plus/es/locale/lang/zh-cn';
 
 // router
