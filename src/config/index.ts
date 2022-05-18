80abf8e73faa71218faafe77fc584bd2f8e4cac7
EightDoor <851708184@qq.com>
2022-05-18 10:30:31 +0800

    fix

--- src/config/index.ts
+++ src/config/index.ts
@@ -3,9 +3,9 @@ const Config = {
   // 七牛云 访问图片的基础地址
   qiuniuLoadUrl: 'http://fanyi.baidu.start6.cn/',
   // 线上地址
-  backUrl: 'https://xka.start6.cn/wx_mn/',
+  // backUrl: 'https://xka.start6.cn/wx_mn/',
   // 本地
-  // backUrl: '/api/',
+  backUrl: '/api/',
 }
 
 export default Config
