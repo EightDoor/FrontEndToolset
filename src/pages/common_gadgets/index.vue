de66abaf912c0826cdc0ef2e1c2d21d977a74013
周凯 <851708184@qq.com>
2023-03-08 22:40:02 +0800

    fix: 网速测试

--- src/pages/common_gadgets/index.vue
+++ src/pages/common_gadgets/index.vue
@@ -10,11 +10,16 @@
 import { markRaw, ref } from 'vue'
 import type { ListType } from '@/pages/gadgets/index.vue'
 import ExchangeRate from '@/pages/common_gadgets/exchangeRate.vue'
+import SpeedTest from '@/pages/common_gadgets/speedTest.vue'
 
 const list = ref<ListType[]>([
   {
     label: '汇率换算',
     component: markRaw(ExchangeRate),
+  },
+  {
+    label: '网速测试',
+    component: markRaw(SpeedTest),
   },
 ])
 </script>
---
+++ src/pages/common_gadgets/speedTest.vue
@@ -0,0 +1,9 @@
+<template>
+  <iframe src="https://test.ustc.edu.cn/" width="100%" height="800px" frameborder="0" />
+</template>
+
+<script lang="ts" setup>
+import { ref } from 'vue'
+</script>
+
+<style scoped lang="less"></style>
