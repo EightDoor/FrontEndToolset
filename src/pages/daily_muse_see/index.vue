14563d2a382dfcd5d40b712e507b74a2220c2bde
eightdoor <851708184@qq.com>
2024-03-15 11:30:16 +0800

    feat: doc hello 算法

--- src/pages/daily_muse_see/index.vue
+++ src/pages/daily_muse_see/index.vue
@@ -9,6 +9,10 @@ import type { ListType } from '@/types/com'
 
 const list = ref<ListType[]>([
   {
+    title: "Hello 算法(动画图解、一键运行的数据结构与算法教程)",
+    url: "https://github.com/krahets/hello-algo"
+  },
+  {
     title: 'github中文排行榜',
     url: 'https://github.com/GrowingGit/GitHub-Chinese-Top-Charts',
     type: '文档',
